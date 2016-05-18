1372b3ad2205185ca72db7e3f6321b721bce9b5a
Vincent Do <do.vincent@live.com>
2016-05-18 17:48:04 -0400

    Add constructor tests

--- jstests/decimal/decimal_constructors.js
+++ jstests/decimal/decimal_constructors.js
@@ -1,26 +1,37 @@
 // Tests constructing NumberDecimal with various types
 
 (function() {
-    "use strict";
-    var col = db.decimal_constructors;
+    'use strict';
+    var col = db.d_constructors;
     col.drop();
 
     // Insert some sample data.
 
     assert.writeOK(col.insert([
-        {"decimal": NumberDecimal("1")},
-        {"decimal": NumberDecimal(1)},
-        {"decimal": NumberDecimal(NumberLong("1"))},
-        {"decimal": NumberDecimal(NumberInt("1"))},
-    ]),
-                   "Initial insertion of decimals failed");
+        {d: NumberDecimal('1')},
+        {d: NumberDecimal(1)},
+        {d: NumberDecimal(NumberLong('1'))},
+        {d: NumberDecimal(NumberInt('1'))},
+        {d: NumberDecimal('NaN')},
+        {d: NumberDecimal('-sNaN')}
+    ]), 'Initial insertion of decimals failed');
+
+    try {
+        col.insert({d: NumberDecimal('some garbage')});
+    } catch(e) {}
+
+    // Not sure why this doesn't work
+    // assert.throws(col.insert, 
+    //     [{d: NumberDecimal('some garbage')}], 'Unexpected success inserting invalid decimal data');
 
     // Find values with various types and NumberDecimal constructed types
-    assert.eq(col.find({"decimal": NumberDecimal("1")}).count(), "4");
-    assert.eq(col.find({"decimal": NumberDecimal(1)}).count(), "4");
-    assert.eq(col.find({"decimal": NumberDecimal(NumberLong(1))}).count(), "4");
-    assert.eq(col.find({"decimal": NumberDecimal(NumberInt(1))}).count(), "4");
-    assert.eq(col.find({"decimal": 1}).count(), "4");
-    assert.eq(col.find({"decimal": NumberLong(1)}).count(), "4");
-    assert.eq(col.find({"decimal": NumberInt(1)}).count(), "4");
+    assert.eq(col.find({'d': NumberDecimal('1')}).count(), '4');
+    assert.eq(col.find({'d': NumberDecimal(1)}).count(), '4');
+    assert.eq(col.find({'d': NumberDecimal(NumberLong(1))}).count(), '4');
+    assert.eq(col.find({'d': NumberDecimal(NumberInt(1))}).count(), '4');
+    assert.eq(col.find({'d': 1}).count(), '4');
+    assert.eq(col.find({'d': NumberLong(1)}).count(), '4');
+    assert.eq(col.find({'d': NumberInt(1)}).count(), '4');
+    // NaN and -sNaN are both evaluated to NaN
+    assert.eq(col.find({'d': NumberDecimal('NaN')}).count(), 2);
 }());
